d5aa3fec057c12d49a709aba786b78f5a550a927
Andrey Yantsen <andrey@janzen.su>
2018-08-30 10:01:50 +0100

    fix installation script for wd-mpwp

--- wd-mypassport-wireless-pro/install.sh
+++ wd-mypassport-wireless-pro/install.sh
@@ -2,15 +2,19 @@
 
 set -e
 
-#ssh root@mypassport.local "mkdir -p /DataVolume/python/"
-#scp $(dirname $0)/python-minimal.zip root@mypassport.local:/DataVolume/python/python.zip
-#ssh root@mypassport.local "cd /DataVolume/python/ && unzip -o python.zip && rm python.zip"
-#ssh root@mypassport.local "curl https://bootstrap.pypa.io/get-pip.py --insecure | PYTHONHOME=/DataVolume/python/ /DataVolume/python/bin/python"
-ssh root@mypassport.local "LC_ALL=en_US.UTF-8 PYTHONHOME=/DataVolume/python/ /DataVolume/python/bin/pip install -U --process-dependency-links plexiglas"
-
+ssh root@mypassport.local "mkdir -p /DataVolume/python/"
 scp $(dirname $0)/plexiglas.service root@mypassport.local:/etc/systemd/system/
-ssh root@mypassport.local "systemctl enable plexiglas.service"
+scp $(dirname $0)/python-minimal.zip root@mypassport.local:/DataVolume/python/python.zip
+
+ssh root@mypassport.local << END
+cd /DataVolume/python/ && unzip -o python.zip && rm python.zip
+curl https://bootstrap.pypa.io/get-pip.py --insecure | PYTHONHOME=/DataVolume/python/ /DataVolume/python/bin/python
+LC_ALL=en_US.UTF-8 PYTHONHOME=/DataVolume/python/ /DataVolume/python/bin/pip install -U --process-dependency-links plexiglas
+END
 
 ssh -t root@mypassport.local "LC_ALL=en_US.UTF-8 PYTHONHOME=/DataVolume/python/ /DataVolume/python/bin/plexiglas -d /DataVolume/PlexSync -i -n 'MyPassport Wireless Pro' -q"
 
-ssh root@mypassport.local "systemctl start plexiglas.service"
+ssh root@mypassport.local << END
+systemctl enable plexiglas.service
+systemctl start plexiglas.service
+END
