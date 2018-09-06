72fb7d9c35238df0f5fc1c8720e369e20084efef
Andrey Yantsen <andrey@janzen.su>
2018-09-06 18:36:15 +0100

    fix typo in setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     readme = f.read()
 
-# Get requirments
+# Get requirements
 requirements = []
 dependency_links = []
 with open('requirements.txt') as handle:
